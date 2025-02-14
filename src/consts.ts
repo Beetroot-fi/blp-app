077aeeb774ba22d82e6847454dba77b54e28f443
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-14 21:19:57 +0500

    feat: add tg btc jetton minter address

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const BLP_JETTON_MINTER = "kQAe9a3XAwGzDksr1QGlizUHLsR3oMtzBC8UeJhvaH9004vq";
+export const TGBTC_JETTON_MINTER = "kQDoy1cUAbGq253vwfoPcqSloODVAWkDBniR12PJFUHnK6Yf";
 export const TESTNET_CHAIN_ID = "-3";

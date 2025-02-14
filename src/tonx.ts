fc2a56c055bb1a075fa7cf1337990469b1b4ca0c
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-14 21:20:11 +0500

    feat: implement tonx api service

---
+++ src/tonx.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance } from "axios";
+
+const api: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+    },
+});
+
+export const apiService = {
+    getJettonBalance: async (walletAddress: string, jettonMinterAddress: string) => {
+        try {
+            const response = await api.post('', {
+                id: 1,
+                jsonrpc: '2.0',
+                method: 'getAccountJettonBalance',
+                params: {
+                    account_id: walletAddress,
+                    jetton_id: jettonMinterAddress
+                }
+            });
+
+            return response.data.result.balance;
+        } catch (error) {
+            throw error;
+        }
+    }
+};

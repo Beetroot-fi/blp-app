75ce6c3e913b3b4368f7ae43e8022d8a173d727b
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-16 21:46:33 +0500

    feat: update blp jetton minter address

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const BLP_JETTON_MINTER = "kQAe9a3XAwGzDksr1QGlizUHLsR3oMtzBC8UeJhvaH9004vq";
+export const BLP_JETTON_MINTER = "kQCAvxdOXK8l_xBfeJ5y-oeAzhyn1OenxhtyTbgge3uQ7TPY";
 export const TGBTC_JETTON_MINTER = "kQDoy1cUAbGq253vwfoPcqSloODVAWkDBniR12PJFUHnK6Yf";
 export const TESTNET_CHAIN_ID = "-3";
